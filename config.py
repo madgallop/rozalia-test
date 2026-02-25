78784298e46d26510410c55076f06fe7ccfb397f
madgallop <mgallop@middlebury.edu>
2026-02-25 12:21:11 -0500

    Update config.py
    Checked all categories against master sheet

--- config.py
+++ config.py
@@ -6,13 +6,12 @@
 
 METADATA_FIELDS = [
     "Date", "Location", "City", "State", 
-    "Latitude", "Longitude", 
     "Type of cleanup", "Type of location", 
     "Distance cleaned (miles)", "Duration (hrs)", 
     "Start time", "End time", "Weather", 
-    "Wind (knots) 0 if none", "Recent weather", 
-    "State of Tide", "Flow", "Recent events (human)", "Total weight (lb)",
-    "# of participants", "Unusual items", "Notes/comments"
+    "Wind (knots)", "Recent weather", 
+    "Tide", "Flow", "Recent events", "Total weight (lb)",
+    "# of participants", "Unusual items", "Notes/comments", "Outlier"
 ]
 
 DEBRIS_GROUPS = {
@@ -67,10 +66,14 @@ DROPDOWN_OPTIONS = {
     "Type of location": ["Sandy Beach", "Rocky Beach", "Private Dock", "Marina", "Park", "Open Water", "River", "Other"],
     "Weather": ["Clear/Sunny", "Rain", "Cloudy", "Windy", "Foggy", "Other"],
     "Recent weather": ["Wind Event", "Rain Event", "Storm Event", "Draught", "None", "Irene", "Other"],
-    "State of Tide": ["High", "Low", "Mid", "Other", "N/A"],
+    "Tide": ["High", "Low", "Mid", "Other", "N/A"],
     "Flow": ["Ebbing", "Flooding", "Slack", "Other", "N/A"],
-    "Recent events (human)": ["Festival", "Holiday", "Concert", "Busy Weekend", "Summer Camp", "None", "Other"],
-    "Times": [
+    "Recent events": ["Festival", "Holiday", "Concert", "Busy Weekend", "Summer Camp", "None", "Other"],
+    "Start time": [
+        "12:00 AM", "12:30 AM", "1:00 AM", "1:30 AM", "2:00 AM", "2:30 AM", "3:00 AM", "3:30 AM", "4:00 AM", "4:30 AM", "5:00 AM", "5:30 AM", "6:00 AM", "6:30 AM", "7:00 AM", "7:30 AM", "8:00 AM", "8:30 AM", "9:00 AM", "9:30 AM", "10:00 AM", "10:30 AM", "11:00 AM", "11:30 AM",
+        "12:00 PM", "12:30 PM", "1:00 PM", "1:30 PM", "2:00 PM", "2:30 PM", "3:00 PM", "3:30 PM", "4:00 PM", "4:30 PM", "5:00 PM", "5:30 PM", "6:00 PM", "6:30 PM", "7:00 PM", "7:30 PM", "8:00 PM", "8:30 PM", "9:00 PM", "9:30 PM", "10:00 PM", "10:30 PM", "11:00 PM", "11:30 PM"
+    ],
+    "End time": [
         "12:00 AM", "12:30 AM", "1:00 AM", "1:30 AM", "2:00 AM", "2:30 AM", "3:00 AM", "3:30 AM", "4:00 AM", "4:30 AM", "5:00 AM", "5:30 AM", "6:00 AM", "6:30 AM", "7:00 AM", "7:30 AM", "8:00 AM", "8:30 AM", "9:00 AM", "9:30 AM", "10:00 AM", "10:30 AM", "11:00 AM", "11:30 AM",
         "12:00 PM", "12:30 PM", "1:00 PM", "1:30 PM", "2:00 PM", "2:30 PM", "3:00 PM", "3:30 PM", "4:00 PM", "4:30 PM", "5:00 PM", "5:30 PM", "6:00 PM", "6:30 PM", "7:00 PM", "7:30 PM", "8:00 PM", "8:30 PM", "9:00 PM", "9:30 PM", "10:00 PM", "10:30 PM", "11:00 PM", "11:30 PM"
     ]
